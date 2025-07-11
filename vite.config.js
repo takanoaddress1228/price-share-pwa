942dae089a10464295bacab69830f7b6be6b7510
takanoaddress1228 <takanoaddress1228@gmail.com>
2025-07-12 01:03:07 +0900

    \"Update vite.config.js outDir to docs\"

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/price-share-pwa/', // この行を追加
+  base: '/price-share-pwa/',
+  build: {
+    outDir: 'docs' // この行を追加
+  }
 })
